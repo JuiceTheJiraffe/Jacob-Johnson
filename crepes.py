a806cd9592ab4fbaa9425726a2ba9002d69762aa
JuiceTheJiraffe <43481491+JuiceTheJiraffe@users.noreply.github.com>
2018-10-25 17:06:11 -0500

    Add files via upload

---
+++ crepes.py
@@ -0,0 +1,10 @@
+#This script tells you how to make crepes
+groceries = ['Eggs', 'Butter', 'Milk', 'Flour'] 
+#Defines the the list of variables in groceries
+howmuch = ['= 2', '= 2 TBSP', '= 1 and a third cups', '= 1 cup']
+#Defines the list of variables in howmuch
+for g in groceries:
+    for h in howmuch:
+        while groceries:
+            while howmuch:
+                print(groceries.pop() + ' ' + howmuch.pop())
